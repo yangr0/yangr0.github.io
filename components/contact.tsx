c1f809f5492a47f5f8f315865b7a8887398bebbc
raymond <ry25@illinois.edu>
2025-01-12 15:43:53 -0600

    added confirmation popup

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,8 +1,12 @@
 "use client";
 
 import "@/components/contact/contact.css";
+import { useState } from "react";
 
 export default function Contact() {
+  const [name, setName] = useState("");
+  const [showConfirmation, setShowConfirmation] = useState(false);
+  const [success, setSuccess] = useState(false);
   async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
     const target = e.target as typeof e.target & {
@@ -10,7 +14,7 @@ export default function Contact() {
       email: { value: string };
       message: { value: string };
     };
-    await fetch("https://api.web3forms.com/submit", {
+    const response = await fetch("https://api.web3forms.com/submit", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -22,7 +26,13 @@ export default function Contact() {
         email: target.email.value,
         message: target.message.value,
       }),
-    });
+    })
+    .then(resp => resp.json().then(data => {
+      console.log(data);
+      setShowConfirmation(true);
+      setName(target.name.value);
+      setSuccess(data.success);
+    }))
   }
   return (
     <>
@@ -42,6 +52,21 @@ export default function Contact() {
           </form>
         </div>
       </section>
+      {showConfirmation && success && (
+        <div className="flex flex-col gap-y-6 fixed h-1/2 w-1/2 top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] border-2 rounded-xl justify-center items-center text-xl bg-black border-stone-300">
+          <h1>I have received your message!</h1>
+          <h1>Thank you for your interest {name}</h1>
+          <h1 className="mb-4">I will get back to you as soon as I see your message</h1>
+          <button className="absolute bottom-[10%] border-[.1rem] border-[#262626] rounded-full px-10 py-2 font-semibold hover:bg-[#151515]" onClick={() => setShowConfirmation(false)}>Close</button>
+        </div>
+      )}
+      {showConfirmation && !success && (
+        <div className="flex flex-col gap-y-6 fixed h-1/2 w-1/2 top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] border-2 rounded-xl justify-center items-center text-xl bg-black border-stone-300">
+          <h1>Message failed to send!</h1>
+          <h1 className="mb-4">Please refresh and try again</h1>
+          <button className="absolute bottom-[10%] border-[.1rem] border-[#262626] rounded-full px-10 py-2 font-semibold hover:bg-[#151515]" onClick={() => setShowConfirmation(false)}>Close</button>
+        </div>
+      )}
     </>
   )
 }
